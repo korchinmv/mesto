51ddb6e669e0a99d8f244457b4f80e6b82a14d87
Maxim <korchinmv@yandex.ru>
2022-12-15 23:43:34 +0300

    fixed all bugs without resolution bugs

--- js/index.js
+++ js/index.js
@@ -1,34 +1,31 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const editButton = document.querySelector(".profile__edit");
-  const popup = document.querySelector(".popup");
-  const closeButton = document.querySelector(".popup__close");
-  const formElement = popup.querySelector(".popup__form");
-  const nameInput = formElement.querySelector(".popup__name");
-  const jobInput = formElement.querySelector(".popup__profession");
-  const profileName = document.querySelector(".profile__name");
-  const profileProfession = document.querySelector(".profile__profession");
+const editButton = document.querySelector(".profile__edit");
+const popup = document.querySelector(".popup");
+const closeButton = document.querySelector(".popup__close");
+const formElement = popup.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".popup__input_js_name");
+const jobInput = formElement.querySelector(".popup__input_js_profession");
+const profileName = document.querySelector(".profile__name");
+const profileProfession = document.querySelector(".profile__profession");
 
-  const popupOpened = () => {
-    popup.classList.add("popup_opened");
-  };
+const popupOpened = () => {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileProfession.textContent;
+};
 
-  const popupClose = () => {
-    popup.classList.remove("popup_opened");
-  };
+const popupClose = () => {
+  popup.classList.remove("popup_opened");
+};
 
-  const handleFormSubmit = (evt) => {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileProfession.textContent = jobInput.value;
+const handleFormSubmit = (evt) => {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileProfession.textContent = jobInput.value;
+  popupClose();
+};
 
-    console.log(profileName);
-    console.log(profileProfession);
-    popupClose();
-  };
-
-  formElement.addEventListener("submit", handleFormSubmit);
-  editButton.addEventListener("click", popupOpened);
-  closeButton.addEventListener("click", popupClose);
-});
+formElement.addEventListener("submit", handleFormSubmit);
+editButton.addEventListener("click", popupOpened);
+closeButton.addEventListener("click", popupClose);
